8df1ed1a5cc7e46fc1be8571720ff4e9c01c8817
sumit9881 <sumit9881@gmail.com>
2016-07-26 08:10:33 -0500

    removed example from package name

--- app/src/androidTest/java/com/example/sadhika/azurelocalsearch/ApplicationTest.java
+++ app/src/androidTest/java/com/example/sadhika/azurelocalsearch/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.sadhika.azurelocalsearch;
+package com.sadhika.azurelocalsearch;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/test/java/com/example/sadhika/azurelocalsearch/ExampleUnitTest.java
+++ app/src/test/java/com/example/sadhika/azurelocalsearch/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.sadhika.azurelocalsearch;
+package com.sadhika.azurelocalsearch;
 
 import org.junit.Test;
 
